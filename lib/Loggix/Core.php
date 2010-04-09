23d801e140ab5d64f08685cd685e6a3559d80e7d
kaz6120 <kaz6120@gmail.com>
2010-04-09 18:50:27 +0900

    Update version number.

--- lib/Loggix/Core.php
+++ lib/Loggix/Core.php
@@ -9,7 +9,7 @@
  * @link      http://loggix.gotdns.org/
  * @license   http://www.opensource.org/licenses/bsd-license.php  New BSD License
  * @since     5.5.16
- * @version   10.4.3
+ * @version   10.4.9
 */
 
 /**
@@ -71,7 +71,7 @@ class Loggix_Core
     const LOGGIX_THEME_DIR        = '/theme/';
     const LOGGIX_TITLE_SEPARATOR  = ' : ';
     const LOGGIX_PROJ_URI         = 'http://loggix.gotdns.org';
-    const LOGGIX_VERSION          = '10.4.3';
+    const LOGGIX_VERSION          = '10.4.9';
     
 
     // {{{ Properties
